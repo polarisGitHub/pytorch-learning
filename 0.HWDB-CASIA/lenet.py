7b8e740375c7a17295a240bbe3b65ad7446feb2b
polaris <polaris1478@gmail.com>
2019-04-05 12:47:22 +0800

    padding

--- 0.HWDB-CASIA/lenet.py
+++ 0.HWDB-CASIA/lenet.py
@@ -8,18 +8,18 @@ from torchsummary import summary
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv1 = nn.Conv2d(1, 6, 5, padding=2)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5, padding=2)
         self.dropout = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(16 * 15 * 15, 120)
+        self.fc1 = nn.Linear(16 * 18 * 18, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 3740)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = self.dropout(x.view(-1, 16 * 15 * 15))
+        x = self.dropout(x.view(-1, 16 * 18 * 18))
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
